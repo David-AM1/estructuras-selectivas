ffce6dcbe3fbd2a42a919f0198184df2e844e62e
David-AM1 <davalosm@unjbg.edu.pe>
2025-05-31 22:13:11 -0500

    Se agrega la posibilidad de volver a intentar probar con otro nnumero en el EJERCICIO 4

--- e4.cpp
+++ e4.cpp
@@ -3,17 +3,22 @@ using namespace std;
 
 int main () {
 	int num_azar;
-	double compra;
+		char respuesta;
+		double compra;
 		cout << "Ingrese el monto de su compra: ";
 		cin >> compra;
+	do{
 		cout << "Ingrese un numero al azar: ";
 		cin >> num_azar;
 		if (num_azar >= 74 ) {
 			compra = compra * 0.8;
-			cout << "Recibio un 20% de descuento, ahora pagara " << compra;
+			cout << "Recibio un 20% de descuento, ahora pagara " << compra<<endl;
 		} else {
 			compra = compra * 0.85;
-			cout << "Recibio un 15% de descuento, ahora pagara " << compra;
+			cout << "Recibio un 15% de descuento, ahora pagara " << compra<<endl;
 		}
-		return 0;
+		cout<<"¿Desea intentar nuevamente con otro numero? (s/n)"<<endl;
+		cin>>respuesta;
+	}while(respuesta=='s');
+	return 0;
 }
